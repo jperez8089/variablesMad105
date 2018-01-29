e60580dbf698011bce5abe634635b85b9ebe17ee
Javier Perez Primary <javierperezprimary@Javiers-MacBook-Pro.local>
2018-01-28 20:58:45 -0600

    Initial commit

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,25 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte b = 3;
+        short sh = 2;
+        int i = 6;
+        long l = 12345302;
+        double dbl =1.234265;
+        float f = (float) 1.14435345346346345235434632523242;
+        boolean bl = true ;
+        char c = 'l' ;
+
+        System.out.println("This is an example of byte: " + b);
+        System.out.println("This is an example of short: " + sh);
+        System.out.println("This is an example of int: " + i);
+        System.out.println("This is an example of long: " + l);
+        System.out.println("This is an example of double: " + dbl);
+        System.out.println("This is an example of float: " + f);
+        System.out.println("This is an example of boolean: " + bl);
+        System.out.println("This is an example of char: " + c);
+    }
+}
